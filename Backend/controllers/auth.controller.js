d418cc5a7fa35551dfef81c40bf3e38564a259bd
ysihak-B <ysihakbazezew@gmail.com>
2024-03-07 23:29:32 +0300

    starting authentication routing and controller

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req, res) => {
+    res.send('Signup Route')
+    console.log('Signup Route')
+}
+export const login = (req, res) => {
+    res.send('Login Route')
+    console.log('Login Route')
+}
+export const logout = (req, res) => {
+    res.send('Logout Route')
+    console.log('Login Route')
+}
