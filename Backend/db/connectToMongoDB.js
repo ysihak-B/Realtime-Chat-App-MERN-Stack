483a75ea036324dbbe3e4ea2ccdd1fba76faf982
ysihak-B <ysihakbazezew@gmail.com>
2024-03-08 18:52:00 +0300

    connecting to mongodb

---
+++ Backend/db/connectToMongoDB.js
@@ -0,0 +1,12 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectToMongoDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log('Connected to MongoDB')
+    } catch (error) {
+        console.log("Error connecting to MongoDB", error.message)
+    }
+}
+
+export default connectToMongoDB;
--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import { config } from 'dotenv'
 import authRoutes from './Routes/auth.routes.js'
+import connectToMongoDB from './db/connectToMongoDB.js'
 
 config()
 const app = express()
 const PORT = process.env.PORT || 3000
+app.use(express.json()) // to parse the incoming request with json payloads ( from req.body )
 
 app.get('/', (req, res) => res.send('Wellcome to Realtime-Chat-App API!'))
 app.use('/api/auth', authRoutes)
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`))
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server listening on port ${PORT}!`)
+})
