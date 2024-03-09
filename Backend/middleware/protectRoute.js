7e603fc075df521fc5818acd012def5626872629
ysihak-B <ysihakbazezew@gmail.com>
2024-03-10 00:14:14 +0300

    adding route protector middleware

---
+++ Backend/middleware/protectRoute.js
@@ -0,0 +1,27 @@
+import jwt from "jsonwebtoken"
+import User from "../models/user.model.js"
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.cookies.jwt
+        if(!token){
+            res.status(401).json({error: "Unauthorized - No token provided"})
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        if(!decoded){
+            res.status(401).json({error: "Unauthorized - Invalid token"})
+        }
+
+        const user = await User.findById(decoded.userId)
+        if(!user){
+            res.status(404).json({error: "User not found"})
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        console.log("Error in protectRoute middleware", error.message)
+        res.status(500).json({error: "Internal server error"})
+    }
+}
